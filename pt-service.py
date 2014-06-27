7a5fdf4e3324012def954b88d6ec74f8c77c20ef
joseph <ptphp@qq.com>
2014-06-27 11:50:33 +0800

    Update pt-service.py

--- pt-service.py
+++ pt-service.py
@@ -5,6 +5,8 @@ pid = os.getpid()
 
 while 1:    
     f = open("/tmp/pt_service.log","a+")
-    f.write(str(pid)+" | "+str(time.time())+"\n")
+    msg = str(pid)+" | "+str(time.time())+" | " + __file__
+    print msg
+    f.write(msg +"\n")
     f.close()
     time.sleep(1)
