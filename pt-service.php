aeae3be527b9c406cdbc9e2d71d5f70de24a1c2a
joseph <ptphp@qq.com>
2014-06-27 11:47:54 +0800

    Update pt-service.php

--- pt-service.php
+++ pt-service.php
@@ -2,7 +2,7 @@
 <?php
 while(1){
     $pid = getmypid();
-    $msg = $pid." | ".date("h:i:s")."\n";
+    $msg = $pid." | ".date("h:i:s")." | ".__file__." "."\n";
     echo $msg;
     file_put_contents("/tmp/pt_service.log",$msg,FILE_APPEND);
     sleep(1);    
